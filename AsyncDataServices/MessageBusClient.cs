7245343ac34b9e3c053ce0e93c2912e4d5430ff7
Carlos Ortega <carlos.ortega@btgcorp.com>
2021-10-07 05:32:15 -0700

    Added .vscode and added MessageBusClient (minus publish).

--- AsyncDataServices/MessageBusClient.cs
+++ AsyncDataServices/MessageBusClient.cs
@@ -1,12 +1,15 @@
 using Microsoft.Extensions.Configuration;
 using Eis.Identity.Api.Dtos;
 using RabbitMQ.Client;
+using System;
 
 namespace Eis.Identity.Api.AsyncDataServices
 {
     public class MessageBusClient : IMessageBusClient
     {
         private readonly IConfiguration _config;
+        private readonly IConnection _conn;
+        private readonly IModel _channel;
 
         public MessageBusClient(IConfiguration config)
         {
@@ -16,11 +19,31 @@ namespace Eis.Identity.Api.AsyncDataServices
                 HostName = _config["RabbitMQHost"], 
                 Port = int.Parse(_config["RabbitMQPort"])
             };
+
+            try 
+            {
+                _conn = factory.CreateConnection();
+                _channel = _conn.CreateModel();
+
+                _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+                _conn.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+
+                Console.WriteLine("--> Connected to messagebus.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"--> Could not connect to MessageBus: {ex.Message}.");
+            }
         }
 
         public void PublishNewPlatform(AppUserPublishedDto abc)
         {
 
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs args) 
+        {
+            Console.WriteLine("--> RabbitMQ connection shutdown.");
         }
     }
 }
